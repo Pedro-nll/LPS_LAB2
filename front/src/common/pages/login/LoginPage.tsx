877f9f7485f8a7231e8560dc8751ec2375c1c229
Pedro-nll <pedronll@outlook.com>
2024-10-04 17:17:19 -0300

    Login fix

--- front/src/common/pages/login/LoginPage.tsx
+++ front/src/common/pages/login/LoginPage.tsx
@@ -1,8 +1,5 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-
-import React from 'react';
 import { useDispatch } from 'react-redux';
 import { User, UserLogin } from '../../helpers/types.ts';
 import { login } from '../../redux/user/slice.js';
@@ -12,78 +9,68 @@ import { Container, Space, Title } from './style.ts';
 import { Button } from 'primereact/button';
 import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
-        
-        
 
 const LoginPage = () => {
     const navigate = useNavigate();
-
-    const [emailIntput, setEmailIntput] = useState('')
-    const [passwordIntput, setpasswordIntput] = useState('')
-
-    const dispatch = useDispatch()
-
+    const dispatch = useDispatch();
     const validade = new Validade();
-    
+
+    const [emailInput, setEmailInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+
     useEffect(() => {
         const localUser = localStorage.getItem("user");
-
-        if (localUser === null) {
-            return
+        if (localUser !== null) {
+            const user = JSON.parse(localUser);
+            dispatch(login(user));
+            navigate('/home');
         }
-        
-        const user = JSON.parse(localUser);
-        dispatch(login(user))
-
-        navigate('/home')
     }, []);
 
     const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log("Q PORRA")
 
-        const email = emailIntput;
-        const password = passwordIntput;
+        const email = emailInput;
+        const password = passwordInput;
         const isEmailValid = validade.validateEmail(email);
-
-        const userLogin : UserLogin = {
-            email: email,
-            password: password,
-        }
 
         if (!isEmailValid) return;
 
-        try {
-            const user : User = await userService.login(userLogin) 
-            localStorage.setItem("user", JSON.stringify(user));
-            dispatch(login(user))
-            navigate("/home")
-        }
-        catch(err) {
-            console.log(err)
-        }
+        const userLogin: UserLogin = {
+            email: email,
+            password: password,
+        };
 
+        try {
+            const user: User = await userService.login(userLogin);
+            localStorage.setItem("user", JSON.stringify(user));
+            dispatch(login(user));
+            navigate("/home");
+        } catch (err) {
+            console.error('Login error:', err);
+            // Handle login error (e.g., show error message to user)
+        }
     };
 
     return (
         <Container>
             <Title>Login</Title>
             <Space value={20}/>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="email">Email</label>
+                <InputText id="email" className='full-width-input' value={emailInput} onChange={(e) => setEmailInput(e.target.value)} />
 
-            <label htmlFor="email">Email</label>
-            <InputText id="email" className='full-width-input' aria-errormessage={''} value={emailIntput} onChange={(e) => setEmailIntput(e.target.value)} />
+                <Space value={15}/>
 
-            <Space value={15}/>
+                <label htmlFor="senha">Senha</label>
+                <Password id="senha" className='full-width-input' value={passwordInput} onChange={(e) => setPasswordInput(e.target.value)} inputClassName='full-width-input' />
 
-            <label htmlFor="senha">Senha</label>
-            <Password id="senha" className='full-width-input' value={passwordIntput} onChange={(e) => setpasswordIntput(e.target.value)} inputClassName='full-width-input' />
+                <Space value={20}/>
 
-            <Space value={20}/>
-
-            <Button label="Login" className='full-width-input' onClick={onSubmit}/>
-            <Space value={5}/>
-            <Button label="Cadastro" className='full-width-input' text onClick={() => navigate('/registration')}/>
-
+                <Button type="submit" label="Login" className='full-width-input' />
+                <Space value={5}/>
+                <Button type="button" label="Cadastro" className='full-width-input' text onClick={() => navigate('/registration')}/>
+            </form>
         </Container>
     );
 };
